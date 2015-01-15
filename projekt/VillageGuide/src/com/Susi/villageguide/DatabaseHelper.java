04665d27bfe530f6f176ed2beecdbda8ee6401b0
gjorsch <g.braeunig@gmail.com>
2015-01-15 16:25:21 +0100

    Revert "ImageButton eingefügt (Karte), klicken auf ListView und Anzeigen in Karte"
    This reverts commit 0f9585e01a8c45b24be1cb92a82f3f7fa95488e5.

--- projekt/VillageGuide/src/com/Susi/villageguide/DatabaseHelper.java
+++ projekt/VillageGuide/src/com/Susi/villageguide/DatabaseHelper.java
@@ -15,7 +15,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	private static String DB_PATH;
 	private static String DB_PATH_PREFIX = "/data/data/";
 	private static String DB_PATH_SUFFIX = "/databases/";
-	private static String DB_NAME = "Niederwiesa.db";
+	private static String DB_NAME = "exercise_database.db";
 	private SQLiteDatabase myDataBase;
 	private final Context myContext;
 
