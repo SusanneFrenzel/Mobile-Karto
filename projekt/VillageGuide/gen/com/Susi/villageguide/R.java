19d5fcbbfff930ad6cc87192e530dd9b5f3201c6
gjorsch <g.braeunig@gmail.com>
2015-01-22 18:40:23 +0100

    Database_xml überarbeitet

--- projekt/VillageGuide/gen/com/Susi/villageguide/R.java
+++ projekt/VillageGuide/gen/com/Susi/villageguide/R.java
@@ -420,12 +420,12 @@ containing a value of this type.
         public static final int star=0x7f02002b;
     }
     public static final class id {
-        public static final int action_list=0x7f070020;
-        public static final int action_map=0x7f07001f;
-        public static final int action_settings=0x7f07001e;
+        public static final int action_list=0x7f070021;
+        public static final int action_map=0x7f070020;
+        public static final int action_settings=0x7f07001f;
         public static final int book_now=0x7f070010;
-        public static final int button1=0x7f070019;
-        public static final int button2=0x7f07001a;
+        public static final int button1=0x7f07001a;
+        public static final int button2=0x7f07001b;
         public static final int buyButton=0x7f07000a;
         public static final int buy_now=0x7f07000f;
         public static final int buy_with_google=0x7f07000e;
@@ -434,11 +434,11 @@ containing a value of this type.
         public static final int holo_dark=0x7f070005;
         public static final int holo_light=0x7f070006;
         public static final int hybrid=0x7f070004;
-        public static final int imageButton1=0x7f070014;
-        public static final int imageButton2=0x7f070015;
-        public static final int imageButton3=0x7f070016;
-        public static final int image_view_1=0x7f070018;
-        public static final int map=0x7f070017;
+        public static final int imageButton1=0x7f070015;
+        public static final int imageButton2=0x7f070016;
+        public static final int imageButton3=0x7f070017;
+        public static final int image_view_1=0x7f070019;
+        public static final int map=0x7f070018;
         public static final int match_parent=0x7f07000c;
         public static final int monochrome=0x7f070013;
         public static final int none=0x7f070000;
@@ -447,11 +447,12 @@ containing a value of this type.
         public static final int sandbox=0x7f070008;
         public static final int satellite=0x7f070002;
         public static final int selectionDetails=0x7f07000b;
-        public static final int snippet=0x7f07001d;
+        public static final int snippet=0x7f07001e;
         public static final int strict_sandbox=0x7f070009;
         public static final int terrain=0x7f070003;
-        public static final int textView1=0x7f07001b;
-        public static final int title=0x7f07001c;
+        public static final int textView1=0x7f07001c;
+        public static final int text_view_1=0x7f070014;
+        public static final int title=0x7f07001d;
         public static final int wrap_content=0x7f07000d;
     }
     public static final class integer {
@@ -581,6 +582,7 @@ containing a value of this type.
         public static final int create_calendar_title=0x7f050004;
         public static final int decline=0x7f050003;
         public static final int list=0x7f05002d;
+        public static final int listactive=0x7f05002e;
         public static final int map=0x7f05002c;
         public static final int store_picture_message=0x7f050001;
         public static final int store_picture_title=0x7f050000;
--- projekt/VillageGuide/src/com/Susi/villageguide/DatabaseActivity.java
+++ projekt/VillageGuide/src/com/Susi/villageguide/DatabaseActivity.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.Susi.villageguide.R.string;
+
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
@@ -23,6 +24,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ExpandableListView.OnChildClickListener;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -34,21 +36,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.widget.ExpandableListView;
 
-
-
 //public class DatabaseActivity extends ExpandableListActivity {
-	public class DatabaseActivity extends ListActivity {
-		
+public class DatabaseActivity extends ListActivity {
 
 	SQLiteDatabase database = null;
 	Cursor dbCursor;
 	DatabaseHelper dbHelper = new DatabaseHelper(this);
-	String kat=null;
-	
-	
-//	private ArrayList<String> parentItems = new ArrayList<String>();
-//	private ArrayList<Object> childItems = new ArrayList<Object>();
-	
+	String kat = null;
+
+	// private ArrayList<String> parentItems = new ArrayList<String>();
+	// private ArrayList<Object> childItems = new ArrayList<Object>();
+
 	// Action Bar
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -60,227 +58,31 @@ import android.widget.ExpandableListView;
 		switch (item.getItemId()) {
 		case R.id.action_map:
 			Intent intentMap = new Intent(this, MapActivity.class);
-			startActivity(intentMap);}
-		switch (item.getItemId()){
+			startActivity(intentMap);
+		}
+		switch (item.getItemId()) {
 		case R.id.action_list:
-			
+
 		}
 		return super.onOptionsItemSelected(item);
 	}
-//	@Override
-//	public void onCreate(Bundle savedInstanceState) {
-//
-//		super.onCreate(savedInstanceState);
-//
-//		// this is not really  necessary as ExpandableListActivity contains an ExpandableList
-//		//setContentView(R.layout.main);
-//
-//		ExpandableListView expandableList = getExpandableListView(); // you can use (ExpandableListView) findViewById(R.id.list)
-//		//ExpandableListView expandableList = (ExpandableListView)findViewById(R.id.list);
-//		expandableList.setDividerHeight(2);
-//		expandableList.setGroupIndicator(null);
-//		expandableList.setClickable(true);
-//
-//		setGroupParents();
-//		setChildData();
-//
-//		MyExpandableAdapter adapter = new MyExpandableAdapter(parentItems, childItems);
-//
-//		adapter.setInflater((LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE), this);
-//		expandableList.setAdapter(adapter);
-//		expandableList.setOnChildClickListener(this);
-//	}
-////		expandableList.setOnChildClickListener(new OnChildClickListener() {
-//			
-//
-//		
-//
-//		@Override
-//		public boolean onChildClick(ExpandableListView parent, View v,
-//				int groupPos, int childPos, long id) {
-//			// TODO Auto-generated method stub
-//			ArrayList<String> selChild = new ArrayList<String>();
-//			selChild.add(childPos, "CP");
-//			selChild.add(groupPos, "GP");
-//			selChild.add((int) id, "ID");
-////			Intent intentMap1 = new Intent(DatabaseActivity.this, MapActivity.class);
-////			String groupPosition=String.valueOf(groupPos);
-////			String childPosition=String.valueOf(childPos);
-////			String ID= String.valueOf(id);
-////			intentMap1.putExtra("GP", groupPosition);
-////			intentMap1.putExtra("CP", childPosition);
-////			intentMap1.putExtra("ID", ID);
-////			startActivity(intentMap1);		
-//			return true;
-//		}		
-//		public void OnExpandableListItemClick(View v){
-//			Intent intentMap1 = new Intent(DatabaseActivity.this, MapActivity.class);
-//			intentMap1.putExtra("CP", 1);
-//			intentMap1.putExtra("GP", 2);
-//			startActivity(intentMap1);
-//		}
-//		
-//	
-//
-//	public void setGroupParents() {
-//		parentItems.add("Hunger");
-//		parentItems.add("Gesundheit");
-//		parentItems.add("Sehenswürdigkeiten");
-//	}
-//
-//	public void setChildData() {
-//
-//		// Hunger
-//		ArrayList<String> child = new ArrayList<String>();
-////		Zugriff auf DB
-//		try {
-//			dbHelper.createDataBase();
-//		} catch (IOException ioe) {
-//		}
-//	
-//		try {
-//			database = dbHelper.getDataBase();
-//			// DB Abfrage
-//			dbCursor = database.rawQuery(
-//					"SELECT Name FROM Hunger;", null);		
-//			dbCursor.moveToFirst();
-//			int index = dbCursor.getColumnIndex("Name");
-//			while (!dbCursor.isAfterLast()) {
-//				String record = dbCursor.getString(index);
-//				child.add(record);
-//				dbCursor.moveToNext();
-//			}
-//		} finally {
-//			if (database != null) {
-//				dbHelper.close();
-//			}
-//		}
-//		childItems.add(child);
-//
-//		// Gesundheit
-//		child = new ArrayList<String>();
-////		Zugriff auf DB
-//		try {
-//			dbHelper.createDataBase();
-//		} catch (IOException ioe) {
-//		}
-//	
-//		try {
-//			database = dbHelper.getDataBase();
-//			// DB Abfrage
-//			dbCursor = database.rawQuery(
-//					"SELECT Name FROM Gesundheit;", null);		
-//			dbCursor.moveToFirst();
-//			int index = dbCursor.getColumnIndex("Name");
-//			while (!dbCursor.isAfterLast()) {
-//				String record = dbCursor.getString(index);
-//				child.add(record);
-//				dbCursor.moveToNext();
-//			}
-//		} finally {
-//			if (database != null) {
-//				dbHelper.close();
-//			}
-//		}
-//		childItems.add(child);
-//		
-//
-//		// Sehenswürdigkeiten
-//		child = new ArrayList<String>();
-////		Zugriff auf DB
-//		try {
-//			dbHelper.createDataBase();
-//		} catch (IOException ioe) {
-//		}
-//	
-//		try {
-//			database = dbHelper.getDataBase();
-//			// DB Abfrage
-//			dbCursor = database.rawQuery(
-//					"SELECT Name FROM Sehenswuerdigkeiten;", null);		
-//			dbCursor.moveToFirst();
-//			int index = dbCursor.getColumnIndex("Name");
-//			while (!dbCursor.isAfterLast()) {
-//				String record = dbCursor.getString(index);
-//				child.add(record);
-//				dbCursor.moveToNext();
-//			}
-//		} finally {
-//			if (database != null) {
-//				dbHelper.close();
-//			}
-//		}
-//		childItems.add(child);
-//
-//	}
-	
-//	public void onListItemClick(ListView l, View v, int pos, long id) {
-//		Log.d("Fehler", String.valueOf(pos));
-//		Log.d("Fehler", String.valueOf(id));
-//
-//		try {
-//			dbHelper.createDataBase();
-//		} catch (IOException ioe) {
-//		}
-//		try {
-//			database = dbHelper.getDataBase();
-//			// DB Abfrage
-//			dbCursor = database.rawQuery(
-//					"SELECT Name, Oeffnungszeiten_Montag_bis_Freitag, Oeffnungszeiten_Samstag, Oeffnungszeiten_Sonntag, Long, Lat FROM Hunger WHERE ID=" + String.valueOf(pos) + ";", null);
-//
-//			dbCursor.moveToFirst();
-//			int index = dbCursor.getColumnIndex("Name");
-//			int index2 = dbCursor.getColumnIndex("Oeffnungszeiten_Montag_bis_Freitag");
-//			int index3 = dbCursor.getColumnIndex("Oeffnungszeiten_Samstag");
-//			int index4 = dbCursor.getColumnIndex("Oeffnungszeiten_Sonntag");
-//			int index5 = dbCursor.getColumnIndex("Long");
-//			int index6 = dbCursor.getColumnIndex("Lat");
-//			while (!dbCursor.isAfterLast()) {
-//				String record = dbCursor.getString(index);
-//				String snippet1 = dbCursor.getString(index2);
-//				String snippet2 = dbCursor.getString(index3);
-//				String snippet3 = dbCursor.getString(index4);
-//				String lgn = dbCursor.getString(index5);
-//				String lat = dbCursor.getString(index6);
-//				dbCursor.moveToNext();
-//				
-//				Intent intentMap1 = new Intent(this, MapActivity.class);
-//				intentMap1.putExtra("Titel", record);
-//				intentMap1.putExtra("OH1", snippet1);
-//				intentMap1.putExtra("OH2", snippet2);
-//				intentMap1.putExtra("OH3", snippet3);
-//				intentMap1.putExtra("Longitude", lgn);
-//				intentMap1.putExtra("Latitude", lat);
-//				startActivity(intentMap1);
-//			}
-//			
-//		} finally {
-//			if (database != null) {
-//				dbHelper.close();
-//			}
-//		}
 
-//	}
-	
-		
-	
-	
-
-	
-	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_database);
-		
+
 	}
 
-// Öffnen des Listviews1
-	
+	// Öffnen des Listviews1
+
 	public void onClickOpenListView1(View view) {
-		kat="Hunger" ;
-//		ListView lv1= (ListView)findViewById(R.id.list1);
-// Zugriff auf DB
+		kat = "Hunger";
+		
+		final TextView textViewToChange = (TextView) findViewById(R.id.text_view_1);
+		textViewToChange.setText("Hunger");
+		// ListView lv1= (ListView)findViewById(R.id.list1);
+		// Zugriff auf DB
 		try {
 			dbHelper.createDataBase();
 		} catch (IOException ioe) {
@@ -289,8 +91,7 @@ import android.widget.ExpandableListView;
 		try {
 			database = dbHelper.getDataBase();
 			// DB Abfrage
-			dbCursor = database.rawQuery(
-					"SELECT Name FROM Hunger;", null);		
+			dbCursor = database.rawQuery("SELECT Name FROM Hunger;", null);
 			dbCursor.moveToFirst();
 			int index = dbCursor.getColumnIndex("Name");
 			while (!dbCursor.isAfterLast()) {
@@ -305,16 +106,17 @@ import android.widget.ExpandableListView;
 		}
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
 				R.layout.list_item, list_values);
-//		lv1.setAdapter(adapter);
+		// lv1.setAdapter(adapter);
 		setListAdapter(adapter);
-		
+
 	}
 
-	
-//Öffnen ListView2
-	
+	// Öffnen ListView2
+
 	public void onClickOpenListView2(View view) {
-		kat="Sehenswuerdigkeiten" ;
+		kat = "Sehenswuerdigkeiten";
+		final TextView textViewToChange = (TextView) findViewById(R.id.text_view_1);
+		textViewToChange.setText("Sehenswürdigkeiten");
 		try {
 			dbHelper.createDataBase();
 		} catch (IOException ioe) {
@@ -324,7 +126,7 @@ import android.widget.ExpandableListView;
 			database = dbHelper.getDataBase();
 			// DB Abfrage
 			dbCursor = database.rawQuery(
-					"SELECT Name FROM Sehenswuerdigkeiten;", null);		
+					"SELECT Name FROM Sehenswuerdigkeiten;", null);
 			dbCursor.moveToFirst();
 			int index = dbCursor.getColumnIndex("Name");
 			while (!dbCursor.isAfterLast()) {
@@ -337,15 +139,18 @@ import android.widget.ExpandableListView;
 				dbHelper.close();
 			}
 		}
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.list_item, list_values);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				R.layout.list_item, list_values);
 		setListAdapter(adapter);
-	
+
 	}
 
-//Öffnen ListView3
+	// Öffnen ListView3
 
 	public void onClickOpenListView3(View view) {
-		kat="Gesundheit" ;
+		kat = "Gesundheit";
+		final TextView textViewToChange = (TextView) findViewById(R.id.text_view_1);
+		textViewToChange.setText("Gesundheit");
 		try {
 			dbHelper.createDataBase();
 		} catch (IOException ioe) {
@@ -354,8 +159,7 @@ import android.widget.ExpandableListView;
 		try {
 			database = dbHelper.getDataBase();
 			// DB Abfrage
-			dbCursor = database.rawQuery(
-					"SELECT Name FROM Gesundheit;", null);		
+			dbCursor = database.rawQuery("SELECT Name FROM Gesundheit;", null);
 			dbCursor.moveToFirst();
 			int index = dbCursor.getColumnIndex("Name");
 			while (!dbCursor.isAfterLast()) {
@@ -368,21 +172,19 @@ import android.widget.ExpandableListView;
 				dbHelper.close();
 			}
 		}
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.list_item, list_values);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				R.layout.list_item, list_values);
 		setListAdapter(adapter);
-		
+
 	}
 
-	
-	
-	
-// Anklicken eines LiestView-Items
+	// Anklicken eines LiestView-Items
 	public void onListItemClick(ListView l, View v, int pos, long id) {
-		
+
 		Log.d("Fehler", String.valueOf(pos));
 		Log.d("Fehler", String.valueOf(id));
-		Log.d("Fehler",String.valueOf(l));
-		Log.d("Fehler",String.valueOf(v));
+		Log.d("Fehler", String.valueOf(l));
+		Log.d("Fehler", String.valueOf(v));
 		Log.d("Kategorie", kat);
 
 		try {
@@ -390,14 +192,22 @@ import android.widget.ExpandableListView;
 		} catch (IOException ioe) {
 		}
 		try {
+			final TextView textViewToChange = (TextView) findViewById(R.id.text_view_1);
+			textViewToChange.setText("");
 			database = dbHelper.getDataBase();
 			// DB Abfrage
-			dbCursor = database.rawQuery(
-					"SELECT Name, Oeffnungszeiten_Montag_bis_Freitag, Oeffnungszeiten_Samstag, Oeffnungszeiten_Sonntag, Long, Lat FROM "+ kat +" WHERE ID=" + String.valueOf(pos) + ";", null);
+			dbCursor = database
+					.rawQuery(
+							"SELECT Name, Oeffnungszeiten_Montag_bis_Freitag, Oeffnungszeiten_Samstag, Oeffnungszeiten_Sonntag, Long, Lat FROM "
+									+ kat
+									+ " WHERE ID="
+									+ String.valueOf(pos)
+									+ ";", null);
 
 			dbCursor.moveToFirst();
 			int index = dbCursor.getColumnIndex("Name");
-			int index2 = dbCursor.getColumnIndex("Oeffnungszeiten_Montag_bis_Freitag");
+			int index2 = dbCursor
+					.getColumnIndex("Oeffnungszeiten_Montag_bis_Freitag");
 			int index3 = dbCursor.getColumnIndex("Oeffnungszeiten_Samstag");
 			int index4 = dbCursor.getColumnIndex("Oeffnungszeiten_Sonntag");
 			int index5 = dbCursor.getColumnIndex("Long");
@@ -411,11 +221,11 @@ import android.widget.ExpandableListView;
 				String lat = dbCursor.getString(index6);
 				Double Long_d = Double.parseDouble(lgn);
 				Double Lat_d = Double.parseDouble(lat);
-				
+
 				dbCursor.moveToNext();
-				
+
 				Intent intentMap1 = new Intent(this, MapActivity.class);
-				Bundle b =new Bundle() ;
+				Bundle b = new Bundle();
 				b.putDouble("Lat", Lat_d);
 				b.putDouble("Long", Long_d);
 				intentMap1.putExtras(b);
@@ -423,11 +233,11 @@ import android.widget.ExpandableListView;
 				intentMap1.putExtra("OH1", snippet1);
 				intentMap1.putExtra("OH2", snippet2);
 				intentMap1.putExtra("OH3", snippet3);
-//				intentMap1.putExtra("Longitude", lgn);
-//				intentMap1.putExtra("Latitude", lat);
+				// intentMap1.putExtra("Longitude", lgn);
+				// intentMap1.putExtra("Latitude", lat);
 				startActivity(intentMap1);
 			}
-			
+
 		} finally {
 			if (database != null) {
 				dbHelper.close();
@@ -435,5 +245,4 @@ import android.widget.ExpandableListView;
 		}
 
 	}
-	}
-
+}
